8d12b5b5edefc55b8e786b23ac99cf7098a99fb2
vmenkov <vmenkov@db74ca9c-5f6d-11de-b3c1-0fd540ce45e4>
2009-09-28 16:15:23 +0000

    paths adjusted
    M    src/edu/dimacs/mms/borj/rcv/qrelToXml.sh
    M    src/edu/dimacs/mms/borj/rcv/rcvToXml.sh

--- src/edu/dimacs/mms/borj/rcv/rcvToXml.sh
+++ src/edu/dimacs/mms/borj/rcv/rcvToXml.sh
@@ -7,7 +7,7 @@ set out=${main}/rcv-out
 set d=${home}/rcv
 set opt="-cp $cp -Dd=${home}/rcv"
 
-set args="borj.rcv.RcvToXML ${out}/rcv-boxer-ids-random.txt ${d}/lyrl2004_tokens_train.dat ${d}/lyrl2004_tokens_test_pt?.dat"
+set args="edu.dimacs.mms.borj.rcv.RcvToXML ${out}/rcv-boxer-ids-random.txt ${d}/lyrl2004_tokens_train.dat ${d}/lyrl2004_tokens_test_pt?.dat"
 # Use -Dqrel=true to also include QREL data to XML
 
 time java $opt -Dout=${out}/train100.xml -Dfrom=1 -Dto=100 $args
