19917873b89134b4156c29802322cacb0c106aa2
vmenkov <vmenkov@db74ca9c-5f6d-11de-b3c1-0fd540ce45e4>
2010-12-05 01:50:15 +0000

    moving some files

---
+++ trunk/sample-data/scripts/findBoxerJar.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+use strict;
+
+#--------------------------------------------------------------------------
+#--- Now we will be looking for boxer.jar, in order to set $BOXER_JAR_PATH,
+#--- if that variable has not been set yet. The expected location is something
+#--- like use-boxer-*/lib/boxer.jar, or boxer-*/lib/boxer.jar; but we don't
+#--- know how far up the directory tree that place may be located!
+#---------------------------------------------------------------------------
+
+
+if (defined $ENV{'BOXER_JAR_PATH'}) {
+	print $ENV{'BOXER_JAR_PATH'};
+} else {
+	print &lookForFile();
+}
+print "\n";
+
+#-- Looks for boxer.jar at various likely locations, and returns an
+#-- absolute path to it. If can't find it, returns an empty string
+
+sub lookForFile() {
+    my $base='boxer.jar';
+    my $maxCnt=7;
+    
+    for(my $cnt=0; $cnt<$maxCnt; $cnt ++) {
+	my $root = `pwd`;
+	$root =~ s/\s+$//;
+	(-r $root) and (-x $root) or     next; 
+	foreach my $test ("$root/$base",  "$root/lib/$base") {
+	    if (-e $test) { return $test; }
+	}
+	
+	opendir(my $dh, $root) || die "can't opendir $root: $!";
+	my @subdirs = grep { /boxer/ && -d "$root/$_" } readdir($dh);
+	closedir $dh;
+	
+	foreach my $s (@subdirs) {
+	    my $loc = "$root/$s";
+	    (-r $loc) or     next; 
+	    foreach my $test ("$loc/$base",  "$loc/lib/$base") {
+		if (-e $test) { return $test; }
+	    }
+        }
+
+	($root ne '/') and chdir( '..') or last;   
+     }
+    return '';
+}
+
--- trunk/sample-data/validation/run-validate2.sh
+++ trunk/sample-data/validation/run-validate2.sh
@@ -1,21 +1,17 @@
 #!/bin/csh
 
-# This script runs BOXER on a small sample
+#-- This script runs BOXER on a small sample. It demonstrates  an
+#-- example of using the "validate" command
 
-source set.sh
+source ../set.sh
 
+echo "Using the following JVM options: ${opt}"
+echo "Reading datasets from $d, suites from $s"
 
 set t=train:${d}/sample.xml
 set v=validate:${d}/sample.xml
 
-#    read-suite:${d}/situation1-fallback-suite.xml \
-
-
 echo "The following run should fail, because the suite is inconsistent"
-time java $opt -Dmodel=tg $driver read-suite:${d}/suite2.xml \
+time java $opt -Dmodel=tg $driver read-suite:${s}/suite2.xml \
    $v  write-suite:suite-out.xml
-
-
-
-
 
