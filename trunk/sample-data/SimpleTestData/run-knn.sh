c149fdfa67a221fc89c145d19567e09225e55d73
vmenkov <vmenkov@db74ca9c-5f6d-11de-b3c1-0fd540ce45e4>
2011-05-31 18:53:50 +0000

    scripts

---
+++ trunk/sample-data/SimpleTestData/run-knn.sh
@@ -0,0 +1,29 @@
+#!/bin/csh
+
+#-- Runs BOXER kNN classifier
+
+echo 0=$0
+
+#-- Setting JVM options (memory used, classpath)
+set conv=edu.dimacs.mms.applications.util.CsvToXml;
+set rep=edu.dimacs.mms.applications.learning.Repeater
+set cp=$HOME/boxer/lib/boxer.jar
+set opt="-Xmx256m -cp ${cp}" 
+
+set k=3
+
+
+set d=$HOME/boxer/out/SimpleTestData/knn
+
+echo Directory $d
+mkdir $d
+if (!(-d $d)) then
+    echo "Failed to create directory $d; please make sure that the path is correct"
+    exit
+endif
+
+#-- Classifier run
+/usr/bin/time java $opt -Dverbosity=1 -Drandom=0 -Dr=264 -DM=264 $rep read-suite:SimpleTestSuite.xml \
+read-learner:knn-learner-param-k=${k}.xml \
+train:SimpleTestData-part-1.xml:$d/train-scores.out  \
+test:SimpleTestData-part-2.xml:$d/test-scores.out   >& $d/knn-k=${k}.log
