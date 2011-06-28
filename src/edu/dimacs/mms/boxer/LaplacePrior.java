28cf6343daac508a38c2d3023848c4d1880ab651
vmenkov <vmenkov@db74ca9c-5f6d-11de-b3c1-0fd540ce45e4>
2011-06-28 16:14:08 +0000

    corrected the variance formula in LaplacePrior

--- src/edu/dimacs/mms/boxer/LaplacePrior.java
+++ src/edu/dimacs/mms/boxer/LaplacePrior.java
@@ -27,15 +27,15 @@ public class LaplacePrior extends Prior {
 
     /** Sets all necessary parameters not set in the parent's
       construction phase. In particularly, computes the Lambda from
-      the (absolute) variance. The formula, lambda = 2/sqrt(var),
-      comes from D.Lewis' 2010-07-08 message. The special cases of 0
+      the (absolute) variance. The formula, lambda = sqrt(2/var),
+      comes from D.Lewis' 2010-07-08 message, corrected 2011-06-23. The special cases of 0
       and Double.POSITIVE_INFINITY are handled as appropriate.
     */
     void complete(Prior base) throws BoxerXMLException {
 	super.complete(base);
 	lambda =  Double.isInfinite(avar)? 0:
 	    (avar == 0) ? Double.POSITIVE_INFINITY :   
-	    2/Math.sqrt(avar);
+	    Math.sqrt(2.0/avar);
     }
 
 
