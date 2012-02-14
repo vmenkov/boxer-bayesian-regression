64659be2f59432b4f7f6b4b09408ea764717fbc3
vmenkov <vmenkov@db74ca9c-5f6d-11de-b3c1-0fd540ce45e4>
2012-02-14 14:40:48 +0000

    support for LAD format:

---
+++ trunk/src/edu/dimacs/mms/borj/rcv/qrelToLad.sh
@@ -0,0 +1,25 @@
+#!/bin/csh
+
+
+set rcv=${home}/rcv
+set main=${home}/boxer
+set out=${main}/rcv-out
+
+set cp=${main}/classes:${main}/lib/xercesImpl.jar
+
+set d=${home}/rcv
+set opt="-cp $cp -Dd=${d} -Dout=${out}"
+
+
+time java $opt -Dqrel=true -Dfrom=1 -Dto=1000 -Dd=${home}/rcv-lad -Dout=out-lad-1.dat \
+edu.dimacs.mms.borj.rcv.RcvToLAD \
+${home}/rcv-lad/regions.sampled.ids \
+${rcv}/lyrl2004_tokens_train.dat 
+
+time java $opt -Dqrel=true -Dfrom=1001 -Dto=2000 -Dd=${home}/rcv-lad -Dout=out-lad-2.dat \
+edu.dimacs.mms.borj.rcv.RcvToLAD \
+${home}/rcv-lad/regions.sampled.ids \
+${rcv}/lyrl2004_tokens_train.dat 
+
+#time java $opt -Dfrom=544179 -Dto=545178 edu.dimacs.mms.borj.rcv.RcvToLAD ${out}/rcv-boxer-ids-random.txt 
+
